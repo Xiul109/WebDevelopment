c3440ddd03af33df738ed6166e33e9a83fc4b1ed
xiul <xiul10901@gmail.com>
2017-04-19 09:52:27 +0200

    Integrated devise

--- genericBook/config/routes.rb
+++ genericBook/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { :registrations => "users/registrations" }
+  
+#  authenticated :user do
+#    root 'secret#index', as: :authenticated_root
+#  end
+
+#  root "devise#sign_in"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
