91d47cf68123dc3630c65b67c85c328efd1be5e2
xiul <xiul10901@gmail.com>
2017-05-05 18:59:20 +0200

    Now you can deny or accept friend petitions

---
+++ genericBook/app/controllers/friendships_controller.rb
@@ -0,0 +1,23 @@
+class FriendshipsController < ApplicationController
+	def deny
+		if friendship=Friendship.find(params[:id]) and friendship.type=="unconfirmed"
+			friendship2=Friendship.find_by(owner: friendship.friend, friend: friendship.owner)
+			friendship.destroy
+			friendship2.destroy
+		else
+			not_found
+		end
+		reload
+	end
+	
+	def accept
+		if friendship=Friendship.find(params[:id]) and friendship.type=="unconfirmed"
+			friendship2=Friendship.find_by(owner: friendship.friend, friend: friendship.owner)
+			friendship.update(type: "confirmed")
+			friendship2.update(type: "confirmed")
+		else
+			not_found
+		end
+		reload
+	end
+end
